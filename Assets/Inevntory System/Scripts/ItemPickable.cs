ef23c887c335c9d7225ea96a09ceabf36e5290e9
MarsPH <m.poorhamidian@gmail.com>
2024-09-23 08:52:48 -0400

    Inventory System Works fine!

--- Assets/Inevntory System/Scripts/ItemPickable.cs
+++ Assets/Inevntory System/Scripts/ItemPickable.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemPickable : MonoBehaviour
+public class ItemPickable : MonoBehaviour, IPickable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ItemSO itemScriptableObjects;
 
-    // Update is called once per frame
-    void Update()
+    public void PickItem()
     {
-        
+        Destroy(gameObject);
     }
 }
--- Assets/Inevntory System/Scripts/PlayerInventory.cs
+++ Assets/Inevntory System/Scripts/PlayerInventory.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
     [Header("General")]//Header
     public List<itemType> inventoryList;//
     public int selectedItem;
+    public float playerReach;
+    [SerializeField] GameObject throwItem_gameobject;
 
     [Space(20)]
     [Header("Keys")]
@@ -19,7 +22,27 @@ public class PlayerInventory : MonoBehaviour
     [SerializeField] GameObject gun_item;
     [SerializeField] GameObject axe_item;
 
+    [Space(20)]
+    [Header("Item prefabs")]
+    [SerializeField] GameObject hammer_prefab;
+    [SerializeField] GameObject gun_prefab;
+    [SerializeField] GameObject axe_prefab;
+
+    [Space(20)]
+    [Header("UI")]
+    [SerializeField] Image[] inventorySlotImage = new Image[9];
+    [SerializeField] Image[] inventoryBackgroundImage = new Image[9];
+    [SerializeField] Sprite emptySlotSprite;
+
+
+
+    [SerializeField] Camera cam;//#2
+    [SerializeField] GameObject pickUpItem_gameObject;
+
     private Dictionary<itemType, GameObject> itemSetActive = new Dictionary<itemType, GameObject>() { };
+    private Dictionary<itemType, GameObject> itemInstantiate = new Dictionary<itemType, GameObject>() { };
+
+
 
     private void Start()
     {
@@ -27,11 +50,81 @@ public class PlayerInventory : MonoBehaviour
         itemSetActive.Add(itemType.Gun, gun_item);
         itemSetActive.Add(itemType.Axe, axe_item);
 
+        itemInstantiate.Add(itemType.Hammer, hammer_prefab);
+        itemInstantiate.Add(itemType.Gun, gun_prefab);
+        itemInstantiate.Add(itemType.Axe, axe_prefab);
+
         NewItemSelected();
     }
 
     void Update()
     {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        Debug.DrawLine(ray.origin, ray.direction);
+        if (Physics.Raycast(ray, out hitInfo, playerReach))
+        {
+            IPickable item = hitInfo.collider.GetComponent<IPickable>();
+            if (item != null)
+            {
+                pickUpItem_gameObject.SetActive(true);
+                if (Input.GetKey(pickItemKey))
+                {
+                    inventoryList.Add(hitInfo.collider.GetComponent<ItemPickable>().itemScriptableObjects.item_type);
+                    item.PickItem();
+                }
+
+            }
+            else
+            {
+                pickUpItem_gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            pickUpItem_gameObject.SetActive(false);
+
+        }
+        // Item throw
+        if (Input.GetKeyDown(throwitemKey) && inventoryList.Count > 0)
+        {
+            Instantiate(itemInstantiate[inventoryList[selectedItem]], position: throwItem_gameobject.transform.position, new Quaternion());
+            inventoryList.RemoveAt(selectedItem);
+            if (selectedItem != 0)
+            {
+                selectedItem -= 1;
+            }
+            NewItemSelected();
+        }
+
+        //UI
+        for (int i = 0; i < 9; i++)
+        {
+            if (i < inventoryList.Count)
+            {
+                inventorySlotImage[i].sprite = itemSetActive[inventoryList[i]].GetComponent<Item>().itemScriptableObject.item_sprite;
+                Debug.Log($"This is {i}");
+            }
+            else
+            {
+                inventorySlotImage[i].sprite = emptySlotSprite;
+            }
+        }
+
+        int a = 0;
+        foreach(Image image in inventoryBackgroundImage)
+        {
+            if (a == selectedItem)
+            {
+                image.color = new Color32(145,255,126,255);
+            }
+            else
+            {
+                image.color = new Color32(219,219,219,255);
+            }
+            a++;
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1) && inventoryList.Count > 0)
         {
             selectedItem = 0;
@@ -85,12 +178,16 @@ public class PlayerInventory : MonoBehaviour
         hammer_item.SetActive(false);
         gun_item.SetActive(false);
         axe_item.SetActive(false);
+        if (inventoryList.Count > 0)
+        {
+            GameObject selectedItemGameObject = itemSetActive[inventoryList[selectedItem]];
+            selectedItemGameObject.SetActive(true);
+        }
 
-        GameObject selectedItemGameObject = itemSetActive[inventoryList[selectedItem]];
-        selectedItemGameObject.SetActive(true);
     }
+}
 
-
-
-
+public interface IPickable
+{
+    void PickItem();
 }
